219d4815119defda46b5a46dbf75a91f6c6c705e
Yan Bueno <buenoy519@gmail.com>
2026-04-29 15:20:21 -0300

    Initial commit

--- Routers/routerHomepage.js
+++ Routers/routerHomepage.js
@@ -5,6 +5,4 @@ const ControllerHomepage = require('../Controllers/controllerHomepage');
 // View para Homepage
 router.get('/', ControllerHomepage.homepage);
 
-router.get('/teste', ControllerHomepage.teste);
-
 module.exports = router;
